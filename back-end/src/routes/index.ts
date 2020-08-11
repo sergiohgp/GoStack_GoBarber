aab30b58385f0ed67ae86532aa823791ff68a505
Sergio Pereira <sergiohgp@gmail.com>
2020-08-11 01:12:26 -0400

    first commit

---
+++ back-end/src/routes/index.ts
@@ -0,0 +1,14 @@
+// src/routes/index.ts
+import { Router, request, response } from 'express'
+import appointmentsRouter from './appointments.routes'
+import usersRouter from './users.routes'
+
+
+
+const routes = Router()
+
+routes.use('/appointments', appointmentsRouter)
+routes.use('/users', usersRouter)
+
+
+export default routes
