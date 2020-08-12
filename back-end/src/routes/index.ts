fb3bfd4f4c8cf550dab926998fa6a2a47b0faa31
Sergio Pereira <sergiohgp@gmail.com>
2020-08-12 03:28:14 -0400

    Sessions created

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,14 +1,17 @@
 // src/routes/index.ts
 import { Router, request, response } from 'express'
+
 import appointmentsRouter from './appointments.routes'
 import usersRouter from './users.routes'
-
+import sessionsRouter from './sessions.routes'
 
 
 const routes = Router()
 
 routes.use('/appointments', appointmentsRouter)
+routes.use('/sessions', sessionsRouter)
 routes.use('/users', usersRouter)
+
 
 
 export default routes
