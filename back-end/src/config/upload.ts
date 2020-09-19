5f76529b777c585b22016b0ccaafda8655be85a3
Sergio Pereira <sergiohgp@gmail.com>
2020-09-19 18:32:26 -0400

    File upload

---
+++ back-end/src/config/upload.ts
@@ -0,0 +1,17 @@
+import { parseWithOptions } from 'date-fns/fp'
+import { request } from 'express'
+import multer from 'multer'
+import path from 'path'
+import crypto from 'crypto'
+
+export default {
+    storage: multer.diskStorage({
+        destination: path.resolve(__dirname, '..', '..', 'tmp'),
+        filename(request, file, callback) {
+            const fileHash = crypto.randomBytes(10).toString('hex')
+            const fileName = `${fileHash}-${file.originalname}`
+
+            return callback(null, fileName)
+        }
+    })
+}
--- back-end/src/routes/users.routes.ts
+++ back-end/src/routes/users.routes.ts
@@ -1,9 +1,14 @@
-import { Router } from 'express'
+import { request, response, Router } from 'express'
+import multer from 'multer'
 
 // import AppointmentsRepository from '../repositories/AppointmentsRepository'
 import CreateUserService from '../services/CreateUserService'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import uploadConfig from '../config/upload'
 
 const usersRouter = Router()
+const upload = multer(uploadConfig)
+
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -23,6 +28,10 @@ usersRouter.post('/', async (request, response) => {
     } catch (err) {
         return response.status(400).json({ error: err.message })
     }
+})
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+    return response.json({ ok: true })
 })
 
 export default usersRouter
