e80824306e7c4a7c4a3944e27d2a7598010c2909
Sergio Pereira <sergiohgp@gmail.com>
2020-09-24 00:51:22 -0400

    DIP implemented to appointments

--- back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,33 +1,35 @@
-import { Router } from 'express'
-import { parseISO } from 'date-fns'
-import { getCustomRepository } from 'typeorm'
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository'
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService'
-import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
-const appointementRouter = Router()
+const appointementsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-appointementRouter.use(ensureAuthenticated)
+appointementsRouter.use(ensureAuthenticated);
 
-appointementRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository)
-  const appointments = await appointmentsRepository.find()
+// appointementsRouter.get('/', async (request, response) => {
+//   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+//   const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments)
+//   return response.json(appointments);
+// });
+
+appointementsRouter.post('/', async (request, response) => {
+
+  const { provider_id, date } = request.body;
+
+  const parsedDate = parseISO(date);
+
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
+
+  const appointment = await createAppointment.execute({ date: parsedDate, provider_id });
+
+  return response.json(appointment);
 })
 
-appointementRouter.post('/', async (request, response) => {
-
-  const { provider_id, date } = request.body
-
-  const parsedDate = parseISO(date)
-
-  const createAppointment = new CreateAppointmentService()
-
-  const appointment = await createAppointment.execute({ date: parsedDate, provider_id })
-
-  return response.json(appointment)
-})
-
-export default appointementRouter
+export default appointementsRouter;
