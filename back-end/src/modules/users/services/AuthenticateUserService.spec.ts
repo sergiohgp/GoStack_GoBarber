60602cde4f6b00a7071732f4ea9c8bfa5b98658f
Sergio Pereira <sergiohgp@gmail.com>
2020-09-28 03:12:50 -0400

    Authenticate test 100%

--- back-end/src/modules/users/services/AuthenticateUserService.spec.ts
+++ back-end/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -5,7 +5,7 @@ import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AppError from '@shared/errors/AppError';
 
 describe('AuthenticateUser', () => {
-  it('should be able to creauthenticate the user', async () => {
+  it('should be able to authenticate the user', async () => {
     const fakeUserRepository = new FakeUserRepository();
 
     const fakeHashProvider = new FakeHashProvider();
@@ -35,5 +35,54 @@ describe('AuthenticateUser', () => {
     expect(response.user).toEqual(user);
   });
 
+  it('should not be able to authenticate a non existing user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '1234'
+      }))
+      .rejects
+      .toBeInstanceOf(AppError);
+  });
+
+  it('should be able to authenticate a user with wrong password', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '1234'
+    });
+
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: 'wrong-password'
+      }))
+      .rejects
+      .toBeInstanceOf(AppError);
+  });
 
 });
