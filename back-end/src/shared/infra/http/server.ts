29d9b736e9acf80bc18035f3c5aa9dcb99d5cf2a
Sergio Pereira <sergiohgp@gmail.com>
2020-10-07 16:44:50 -0400

    Cors to connect to front end

--- back-end/src/shared/infra/http/server.ts
+++ back-end/src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -11,6 +12,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
