d32cd42191e7caf11fd3ac5bf2453cebe0cd7b98
Sergio Pereira <sergiohgp@gmail.com>
2020-10-28 00:52:06 -0400

    Amazon s3 upload implemented

--- back-end/src/modules/users/infra/http/routes/users.routes.ts
+++ back-end/src/modules/users/infra/http/routes/users.routes.ts
@@ -11,7 +11,7 @@ const usersRouter = Router();
 const usersController = new UsersController();
 const usersAvatarController = new UsersAvatarController();
 
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post(
   '/',
--- back-end/src/shared/container/providers/StorageProvider/index.ts
+++ back-end/src/shared/container/providers/StorageProvider/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import uploadConfig from '@config/upload';
+
 import IStorageProvider from './models/IStorageProvider';
 import DiskStorageProvider from './implementations/DiskStorageProvider';
 import S3StorageProvider from './implementations/S3StorageProvider';
@@ -9,4 +11,7 @@ const providers = {
   s3: S3StorageProvider,
 };
 
-container.registerSingleton<IStorageProvider>('StorageProvider', providers.s3);
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  providers[uploadConfig.driver],
+);
